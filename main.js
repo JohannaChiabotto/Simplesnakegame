d878948302a9fb96a1f45972088cc3ad29dabe13
johannachiabotto <johanna.chiabotto@gmail.com>
2022-07-05 13:23:51 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let name = 'johanna';
+let arrayXy = [['apple','banana','kiwi'], ['schwein', 'kuh']];
+
+let obj = {obst: ['apple','banana','kiwi'], fleisch: ['schwein', 'kuh']};
+
+name = 'Gabriella';
+
+console.log(arrayXy[0]);
+console.log(obj.obst);
